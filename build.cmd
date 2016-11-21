1f66bb1bb5441469e2a1b6a4e3cf1b2610e7bad2
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-11-21 16:46:31 +0100

    Try to use post deployment action

--- build.cmd
+++ build.cmd
@@ -11,5 +11,3 @@ if errorlevel 1 (
   exit /b %errorlevel%
 )
 
-call :ExecuteCmd "%KUDU_SYNC_CMD%" -v 50 -f "%DEPLOYMENT_TEMP%" -t "%DEPLOYMENT_TARGET%" -n "%NEXT_MANIFEST_PATH%" -p "%PREVIOUS_MANIFEST_PATH%" -i ".git;.hg;.deployment;deploy.cmd"
-IF !ERRORLEVEL! NEQ 0 goto error
