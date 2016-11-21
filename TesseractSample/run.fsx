b19d5488a5e3aec43641aae2bd4d8acc9cbd5fff
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-11-21 14:49:12 +0100

    Add Paket

--- TesseractSample/run.fsx
+++ TesseractSample/run.fsx
@@ -1,14 +1,25 @@
 #r "System.Net.Http"
 #r "System.Net.Http.Formatting"
 #r "System.Web.Http"
+#r "../packages/Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
+
 
 open System.Net
 open System.Net.Http
+open Newtonsoft.Json
+
+
+type Message = {
+    name : string
+}
 
 
 let proc (req : HttpRequestMessage) = async {
     let! ctn = req.Content.ReadAsStringAsync() |> Async.AwaitTask
-    return req.CreateResponse(HttpStatusCode.OK, "Hello" + ctn);
+    let msg = JsonConvert.DeserializeObject<Message> ctn
+
+
+    return req.CreateResponse(HttpStatusCode.OK, "Hello " + msg.name);
 }
 
 let Run(req: HttpRequestMessage) =
