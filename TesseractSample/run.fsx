3030d64c6cef141f69a1b8c39bfe3bf9bc75dda8
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-11-21 16:10:30 +0100

    Try to add tesseract

--- TesseractSample/run.fsx
+++ TesseractSample/run.fsx
@@ -1,25 +1,39 @@
 #r "System.Net.Http"
 #r "System.Net.Http.Formatting"
 #r "System.Web.Http"
+#r "System.Drawing"
 #r "../packages/Newtonsoft.Json/lib/net45/Newtonsoft.Json.dll"
+#r "../packages/Tesseract/lib/net45/Tesseract.dll"
+
+System.Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
 
 open System.Net
 open System.Net.Http
 open Newtonsoft.Json
+open Tesseract
 
 
 type Message = {
     name : string
 }
 
+let ocr input =
+    use engine = new TesseractEngine("tessdata", "eng", EngineMode.Default)
+    use img = Pix.LoadFromFile("phototest.tif")
+    use page = engine.Process img
+    page.GetText()
+
 
 let proc (req : HttpRequestMessage) = async {
+    let text = ocr()
+
     let! ctn = req.Content.ReadAsStringAsync() |> Async.AwaitTask
     let msg = JsonConvert.DeserializeObject<Message> ctn
 
+    let output = sprintf "Hello %s, \nHere is your text: \n%s" msg.name text
 
-    return req.CreateResponse(HttpStatusCode.OK, "Hello " + msg.name);
+    return req.CreateResponse(HttpStatusCode.OK, output);
 }
 
 let Run(req: HttpRequestMessage) =
